83bcf5710ddbc1f5f6692950bb96c8d1ed72e95e
Subhash kumar <95863343+codersubhji@users.noreply.github.com>
2025-08-15 14:51:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001"
+    ],
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/frontend", express.static(path.join(__dirname, "frontend")));
+
+// Debug logs (remove in production)
+console.log("CLIENT_ID:", process.env.CLIENT_ID);
+console.log("REDIRECT_URI:", process.env.REDIRECT_URI);
+console.log("CLIENT_SECRET:", process.env.CLIENT_SECRET ? "Loaded" : "Missing");
+console.log("TENANT_ID:", process.env.TENANT_ID);
+
+// ✅ Test API route
+app.get("/test", (req, res) => {
+  res.json({
+    status: "success",
+    message: "API is running 🚀",
+    timestamp: new Date()
+  });
+});
+
+const authRouter = require("./routes/auth");
+app.use("/", authRouter);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
